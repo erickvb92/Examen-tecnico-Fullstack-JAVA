da20bcf43adfc8ffa44ae957481d44f3888786a1
erickvb92 <90078471+erickvb92@users.noreply.github.com>
2023-06-29 21:23:32 -0600

    Aplicacion jar para el consumo de servicios de marvel
    Creacion de la aplicacion que se empaquetara en jar y consumira los servicios de marvel

---
+++ jaraplication/src/main/java/com/openpay/jaraplication/JaraplicationApplication.java
@@ -0,0 +1,13 @@
+package com.openpay.jaraplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaraplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaraplicationApplication.class, args);
+	}
+
+}
---
+++ jaraplication/src/test/java/com/openpay/jaraplication/JaraplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpay.jaraplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaraplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
